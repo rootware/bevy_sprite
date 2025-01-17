a633f9d4fd45abde54ae2e9f6dfcfc9fffcb9764
rootware <Shah.Saad.Alam@rice.edu>
2025-01-17 01:23:00 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 //! Renders a 2D scene containing a single, moving sprite.
 
-use bevy::{diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, prelude::*, window::{PresentMode, WindowResolution}};
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{PresentMode, WindowResolution},
+};
 
 fn main() {
     App::new()
@@ -17,7 +21,6 @@ fn main() {
             FrameTimeDiagnosticsPlugin,
             LogDiagnosticsPlugin::default(),
         ))
-
         .add_systems(Startup, setup)
         .add_systems(Update, sprite_movement)
         .run();
@@ -32,23 +35,23 @@ enum Direction {
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
     commands.spawn((
-        Sprite::from_image(asset_server.load("ghost.png")),
+        Sprite::from_image(asset_server.load("pacman.png")),
         Transform::from_xyz(100., 0., 0.),
         Direction::Up,
     ));
 }
 
-pub const actions : [i32;5] = [0,1,0,2,3];
+pub const actions: [i32; 15] = [0, 1, 0, 2, 3, 3, 3, 3, 4, 0, 1, 2, 1, 5, 10];
 /// The sprite is animated by changing its translation depending on the time that has passed since
 /// the last frame.
 fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
     for (mut logo, mut transform) in &mut sprite_position {
-      //  match *logo {
-      //      Direction::Up => transform.translation.y += 150. * time.delta_secs(),
-      //      Direction::Down => transform.translation.y -= 150. * time.delta_secs(),
-      //  }
+        //  match *logo {
+        //      Direction::Up => transform.translation.y += 150. * time.delta_secs(),
+        //      Direction::Down => transform.translation.y -= 150. * time.delta_secs(),
+        //  }
 
-        let i = ( time.elapsed_secs() as f64/time.delta_secs_f64() ) as usize % 5;
+        let i = (time.elapsed_secs() as f64 / time.delta_secs_f64()) as usize % 15;
         let my_move = actions[i];
 
         match my_move {
@@ -58,7 +61,6 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
             3 => transform.translation.x -= 1.,
             _ => (),
         }
-
 
         if transform.translation.y > 540. {
             transform.translation.y = 0.;
