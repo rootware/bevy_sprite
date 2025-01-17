f2fc94b8d05de506c7907ce5cc4b1d6d7f42bb0c
rootware <Shah.Saad.Alam@rice.edu>
2025-01-17 01:15:14 -0700

    commit bevy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 //! Renders a 2D scene containing a single, moving sprite.
 
-use bevy::prelude::*;
+use bevy::{diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, prelude::*, window::{PresentMode, WindowResolution}};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    present_mode: PresentMode::AutoNoVsync,
+                    resolution: WindowResolution::new(1920.0, 1080.0)
+                        .with_scale_factor_override(1.0),
+                    ..default()
+                }),
+                ..default()
+            }),
+            FrameTimeDiagnosticsPlugin,
+            LogDiagnosticsPlugin::default(),
+        ))
+
         .add_systems(Startup, setup)
         .add_systems(Update, sprite_movement)
         .run();
@@ -14,39 +27,43 @@ fn main() {
 enum Direction {
     Up,
     Down,
-    Left,
-    Right
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
     commands.spawn((
-        Sprite::from_image(asset_server.load("mario.png")),
+        Sprite::from_image(asset_server.load("car.png")),
         Transform::from_xyz(100., 0., 0.),
         Direction::Up,
     ));
 }
 
+pub const actions : [i32;5] = [0,1,0,2,3];
 /// The sprite is animated by changing its translation depending on the time that has passed since
 /// the last frame.
 fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut logo, mut transform) in &mut sprite_position {
+      //  match *logo {
+      //      Direction::Up => transform.translation.y += 150. * time.delta_secs(),
+      //      Direction::Down => transform.translation.y -= 150. * time.delta_secs(),
+      //  }
 
-    let action_record = [ 0].to_vec();
-    let mut enums = Vec::<Direction>::new();
-    let (mut logo, mut transform) = sprite_position.single_mut();
-    for a in action_record {
-        match a {
-            0 => transform.translation.y += 100. * time.delta_secs(),
-            1 => transform.translation.y -= 100. * time.delta_secs(),
-            2 => transform.translation.x += 100. * time.delta_secs(),
-            3 => transform.translation.x -= 100. * time.delta_secs(),
+        let i = ( time.elapsed_secs() as f64/time.delta_secs_f64() ) as usize % 5;
+        let my_move = actions[i];
+
+        match my_move {
+            0 => transform.translation.y += 1.,
+            1 => transform.translation.y -= 1.,
+            2 => transform.translation.x += 1.,
+            3 => transform.translation.x -= 1.,
             _ => (),
         }
+
 
         if transform.translation.y > 200. {
             transform.translation.y = 0.;
         } else if transform.translation.y < -200. {
-            transform.translation.y = 0.
+            transform.translation.y = 0.;
         }
 
         if transform.translation.x > 200. {
@@ -54,60 +71,5 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
         } else if transform.translation.x < -200. {
             transform.translation.x = 0.;
         }
-
     }
 }
-    /* 
-
-
-    for a in enums {
-
-        let (mut logo, mut transform) = sprite_position.get_single_mut().unwrap();
-
-        *logo = a;
-        match *logo {
-            Direction::Up => transform.translation.y += 100. * time.delta_secs(),
-            Direction::Down => transform.translation.y -= 100. * time.delta_secs(),
-            Direction::Right => transform.translation.x += 100. * time.delta_secs(),
-            Direction::Left => transform.translation.x -= 100. * time.delta_secs(),
-        }
-
-        if transform.translation.y > 200. {
-            *logo = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *logo = Direction::Up;
-        }
-
-        if transform.translation.x > 200. {
-            *logo = Direction::Left;
-        } else if transform.translation.x < -200. {
-            *logo = Direction::Right;
-        }
-
-        
-    }
-
-    }
-    */
-    /* 
-    for (mut logo, mut transform) in &mut sprite_position {
-        match *logo {
-            Direction::Up => transform.translation.y += 100. * time.delta_secs(),
-            Direction::Down => transform.translation.y -= 100. * time.delta_secs(),
-            Direction::Right => transform.translation.x += 100. * time.delta_secs(),
-            Direction::Left => transform.translation.x -= 100. * time.delta_secs(),
-        }
-
-        if transform.translation.y > 200. {
-            *logo = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *logo = Direction::Up;
-        }
-
-        if transform.translation.x > 200. {
-            *logo = Direction::Left;
-        } else if transform.translation.x < -200. {
-            *logo = Direction::Right;
-        }
-    }
-    */
